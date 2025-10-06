c7a81e70c5bd76784858816bd4df7e955e579874
DrSKPunk <anime.naave@mail.ru>
2025-10-06 10:59:57 +0700

    Add files via upload

--- FilmO.java
+++ FilmO.java
@@ -42,22 +42,31 @@ class Kinofilm {
 
 class Studio {
     private String name;
-    private Kinofilm[] films;
-    private int[] sbory;
+    private Kinofilm film1;
+    private Kinofilm film2;
+    private Kinofilm film3;
+    private int sbory1;
+    private int sbory2;
+    private int sbory3;
 
     public Studio() {
         this.name = "";
-        this.films = new Kinofilm[3];
-        this.sbory = new int[3];
-        for (int i = 0; i < 3; i++) {
-            films[i] = new Kinofilm();
-        }
+        this.film1 = new Kinofilm();
+        this.film2 = new Kinofilm();
+        this.film3 = new Kinofilm();
+        this.sbory1 = 0;
+        this.sbory2 = 0;
+        this.sbory3 = 0;
     }
 
-    public Studio(String name, Kinofilm[] films, int[] sbory) {
+    public Studio(String name, Kinofilm film1, Kinofilm film2, Kinofilm film3, int sbory1, int sbory2, int sbory3) {
         this.name = name;
-        this.films = films;
-        this.sbory = sbory;
+        this.film1 = film1;
+        this.film2 = film2;
+        this.film3 = film3;
+        this.sbory1 = sbory1;
+        this.sbory2 = sbory2;
+        this.sbory3 = sbory3;
     }
 
     public void read() {
@@ -66,26 +75,44 @@ class Studio {
         this.name = scanner.nextLine();
 
         System.out.println("Введите данные о 3 фильмах:");
-        for (int i = 0; i < 3; i++) {
-            System.out.println("Фильм " + (i + 1) + ":");
-            films[i].read();
-        }
+
+        System.out.println("Фильм 1:");
+        this.film1 = new Kinofilm();
+        film1.read();
+
+        System.out.println("Фильм 2:");
+        this.film2 = new Kinofilm();
+        film2.read();
+
+        System.out.println("Фильм 3:");
+        this.film3 = new Kinofilm();
+        film3.read();
 
         System.out.println("Введите доход от проката в других странах для 3 фильмов:");
-        for (int i = 0; i < 3; i++) {
-            System.out.print("Доход для фильма " + (i + 1) + ": ");
-            sbory[i] = scanner.nextInt();
-        }
+        System.out.print("Доход для фильма 1: ");
+        this.sbory1 = scanner.nextInt();
+        System.out.print("Доход для фильма 2: ");
+        this.sbory2 = scanner.nextInt();
+        System.out.print("Доход для фильма 3: ");
+        this.sbory3 = scanner.nextInt();
     }
 
     public void display() {
         System.out.println("Киностудия: " + name);
         System.out.println("Фильмы:");
-        for (int i = 0; i < 3; i++) {
-            System.out.print("Фильм " + (i + 1) + ": ");
-            films[i].display();
-            System.out.println("Доход от проката в других странах: " + sbory[i]);
-        }
+
+        System.out.print("Фильм 1: ");
+        film1.display();
+        System.out.println("Доход от проката в других странах: " + sbory1);
+
+        System.out.print("Фильм 2: ");
+        film2.display();
+        System.out.println("Доход от проката в других странах: " + sbory2);
+
+        System.out.print("Фильм 3: ");
+        film3.display();
+        System.out.println("Доход от проката в других странах: " + sbory3);
+
         System.out.println("Суммарный доход киностудии: " + calculate());
 
         Kinofilm mostProfitable = coolfilm();
@@ -94,27 +121,23 @@ class Studio {
     }
 
     public int calculate() {
-        int total = 0;
-        for (int i = 0; i < 3; i++) {
-            total += films[i].getDohod() + sbory[i];
-        }
+        int total = film1.getDohod() + film2.getDohod() + film3.getDohod() +
+                sbory1 + sbory2 + sbory3;
         return total;
     }
 
     public Kinofilm coolfilm() {
-        double maxF = 0;
-        int mostF = 0;
+        double profit1 = ((double) (film1.getDohod() + sbory1) / film1.getTraty()) * 100;
+        double profit2 = ((double) (film2.getDohod() + sbory2) / film2.getTraty()) * 100;
+        double profit3 = ((double) (film3.getDohod() + sbory3) / film3.getTraty()) * 100;
 
-        for (int i = 0; i < 3; i++) {
-            int totalRevenue = films[i].getDohod() + sbory[i];
-            double profitability = ((double) totalRevenue / films[i].getTraty()) * 100;
-
-            if (profitability > maxF) {
-                maxF = profitability;
-                mostF = i;
-            }
+        if (profit1 >= profit2 && profit1 >= profit3) {
+            return film1;
+        } else if (profit2 >= profit1 && profit2 >= profit3) {
+            return film2;
+        } else {
+            return film3;
         }
-        return films[mostF];
     }
 }
 
@@ -133,15 +156,11 @@ public class FilmO {
         studio.read();
         studio.display();
 
-        Kinofilm[] films = {
-                new Kinofilm(1000000, 1500000),
-                new Kinofilm(2000000, 3500000),
-                new Kinofilm(3000000, 2500000)
-        };
+        Kinofilm f1 = new Kinofilm(1000000, 1500000);
+        Kinofilm f2 = new Kinofilm(2000000, 3500000);
+        Kinofilm f3 = new Kinofilm(3000000, 2500000);
 
-        int[] foreignRevenue = {500000, 1000000, 2000000};
-
-        Studio studio2 = new Studio("20ВЕКФОКС", films, foreignRevenue);
+        Studio studio2 = new Studio("20ВЕКФОКС", f1, f2, f3, 500000, 1000000, 2000000);
         studio2.display();
 
         scanner.close();
